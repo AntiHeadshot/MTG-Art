e4ff552cd6d0bead583faf90933991fee0c12302
AntiHeadshot <github@dahmen.space>
2025-08-30 22:18:49 +0200

    update Print Preview Creation

--- src/previewCreation.js
+++ src/previewCreation.js
@@ -9,11 +9,11 @@ class ImageDocumentPreview {
         this.cards = [];
     }
 
-    addImage(count, card) {
+    addCard(count, card) {
         this.cards.push({ count, card });
     }
 
-    async create(updateCallback) {
+    create(updateCallback) {
         const ptToPx = 1.333;
 
         let settings = getSettings(this.options, ptToPx);
@@ -22,47 +22,55 @@ class ImageDocumentPreview {
 
         svg.setAttribute('version', '1.1');
         svg.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
-        svg.setAttribute('viewBox', `0 0 ${this.options.pageWidth} ${this.options.pageHeight}`);
+        svg.setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink');
+        svg.setAttribute('preserveAspectRatio', 'xMidYMid meet');
 
         var imgNr = 0;
 
-        let cards = this.cards.flatMap(card => Array(card.count).flatMap(() => card.card.imageUris.map((u, i) => ({ imageUri: u, highResImageUri: card.card.highResImageUris[i] }))));
+        let cards1 = this.cards.map(card => Array(card.count).fill(card.card)).flat(1);
+        let cards2 = cards1.map(card => card.imageUris.map((u, i) => ({ card, u, i }))).flat(1);
+        let cards = cards2.map(x => ({ imageUri: x.u, highResImageUri: x.card.highResImageUris[x.i] }));
 
         var maxStep = cards.length * 5;
 
-        let dataUrl;
         let pageNr = 0;
         let progress = 0;
 
         updateCallback(0);
 
         while (imgNr < cards.length) {
+            pageNr++;
 
-            let page = svg.createElement('rect');
+            let pageGroup = document.createElement('g');
+            pageGroup.setAttribute('id', `page${pageNr}`);
+            pageGroup.setAttribute('transform', `translate(0,${(pageNr - 1) * (settings.pageHeight + 16)})`);
+            svg.appendChild(pageGroup);
+
+            let page = document.createElement('rect');
 
             page.setAttribute('stroke', 'black');
+            page.setAttribute('fill', 'white');
             page.setAttribute('width', settings.pageWidth);
             page.setAttribute('height', settings.pageHeight);
-            page.setAttribute('y', pageNr * (settings.pageHeight + 16));
 
-            svg.childNodes.push(page);
+            pageGroup.appendChild(page);
 
             for (let y = 0, yPos = settings.marginY; y < settings.yCnt && imgNr < cards.length; y++, yPos += settings.mtgHeight + settings.cardMargin) {
                 for (let x = 0, xPos = settings.marginX; x < settings.xCnt && imgNr < cards.length; x++, xPos += settings.mtgWidth + settings.cardMargin, imgNr++) {
 
                     let card = cards[imgNr];
 
-                    let img = (dataUrl, xPos, yPos, { width: settings.mtgWidth, height: settings.mtgHeight });
+                    let img = document.createElement('image');
 
                     img.setAttribute('x', xPos);
                     img.setAttribute('y', yPos);
                     img.setAttribute('width', settings.mtgWidth);
                     img.setAttribute('height', settings.mtgHeight);
-                    img.setAttribute('href', card.imageUri);
+                    img.setAttribute('xlink:href', card.imageUri);
 
                     //getDataUrl(card.highResImageUri).then(dataUrl => { img.setAttribute('href', dataUrl); });
 
-                    page.appendChild(img);
+                    pageGroup.appendChild(img);
 
                     progress++;
                     updateCallback(progress / maxStep);
@@ -81,7 +89,7 @@ class ImageDocumentPreview {
                                         let inset = cl_2 * .9;
                                         let insetO = cl_2 - inset;
 
-                                        let star = svg.createElement('path');
+                                        let star = document.createElement('path');
                                         star.setAttribute('stroke-width', '0');
                                         star.setAttribute('fill', settings.cropMarkColor);
                                         star.setAttribute('d', `M ${xPos - cl_2},${yPos} `
@@ -90,13 +98,13 @@ class ImageDocumentPreview {
                                             + `c -${inset},-${insetO} -${inset},-${insetO} -${cl_2},-${cl_2} `
                                             + `c -${insetO},${inset} -${insetO},${inset} -${cl_2},${cl_2}`);
 
-                                        page.appendChild(star);
+                                        pageGroup.appendChild(star);
                                     }
                                     break;
                                 case CropMark.LINES:
                                 default:
                                     {
-                                        let line = svg.createElement('path');
+                                        let line = document.createElement('path');
 
                                         line.setAttribute('stroke-width', settings.cropMarkWidth);
                                         line.setAttribute('stroke', settings.cropMarkColor);
@@ -104,16 +112,20 @@ class ImageDocumentPreview {
                                         line.setAttribute('fill', 'transparent');
                                         line.setAttribute('d', `M${xPos},${yPos - cl_2} v${settings.cropMarkSize} M${xPos - cl_2},${yPos} h${settings.cropMarkSize}`);
 
-                                        page.appendChild(line);
+                                        pageGroup.appendChild(line);
                                     }
                                     break;
                             }
                         }
                     }
                 }
-
-            pageNr++;
         }
+
+        svg.setAttribute('viewBox', `0 0 ${settings.pageWidth} ${pageNr * (settings.pageHeight + 16) - 16}`);
+        svg.setAttribute('width', settings.pageWidth);
+        svg.setAttribute('height', pageNr * (settings.pageHeight + 16) - 16);
+        svg.setAttribute('style', 'width:100%; height:auto;');
+        
         return svg;
     }
 }
